6cf79c48fc8762a025b097d41e3bb0873b8ed400
unknown <stevemt@.ucm.ColoState.EDU>
2010-08-19 16:06:40 -0600

    monster direction and state changes beginning implementation, also increased board size, can be any size (granted needs to have open space for player and monsters)

--- mp.js
+++ mp.js
@@ -5,34 +5,48 @@
 // - Fix off by one calculation of position
 ////////////////////////////////////////
 
-//globals 
-function log(msg) { if (console.log) { console.log(msg); } }
-function jqdiv(id, cls, str, x, y, w, h) {
-    return $("<div/>", { 'id': id, 'class': cls, 'html': str }).css({ left: x, top: y, width: w, height: h });
+//globals
+function log(msg) {
+    if (console.log) {
+        console.log(msg);
+    }
 }
 
-function update_hud(health, rocks, keys, room) {
+function rnd(min, max) {    
+    return min + Math.floor(Math.random() * (max - min)); 
+}
+
+function jqdiv(id, cls, str, x, y, w, h) {
+    return $("<div/>", { 'id': id, 'class': cls, 'html': str }).css({ left: x, top: y, width: w, height: h }); 
+}
+
+function update_hud(health, rocks, keys, room) { 
     $("#health").html(health);
-    $("#health").html(rocks);
-    $("#health").html(keys);
-    $("#health").html(room);
+    $("#rocks").html(rocks);
+    $("#keys").html(keys);
+    $("#rooms").html(room);
 }
 
 var 
 M = Math,
 currentTime = new Date(),
-directions = { STILL: 0, LEFT: 1, UP: 2, RIGHT: 3, DOWN: 4 },
-spritestates = { ALIVE: 0, DEAD: 1 },
+directions = { STILL: 0, LEFT: 1, UP: 2, RIGHT: 3, DOWN: 4 }, 
+n_directions = 5,
+spritestates = { ALIVE: 0, DEAD: 1 }, 
+n_spritestates = 2,
 aistates = { RANDOM: 0, CHASE: 1, EVADE: 2, STILL: 3, PATTERN: 4, EXPLODE: 5 },
+n_aistates = 6,
 gamestates = { RUNNING: 0, PAUSED: 1 },
+n_gamestates = 2,
 tiletypes = { WALL: 0, DOOR: 1, FLOOR: 2, KEY: 3, ROCKBAG: 4 },
 keycodes = { SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40, P: 80, A: 65, W: 87, D: 68, S: 83 },
 keys = [],
 cellsize = 32,
-rows = 9,
-cols = 16,
+rows = 15,
+cols = 15,
 numRooms = rows * cols,
 numMonsters = 5,
+difficulty = 1, // difficulty 1-5 (should multiply # of monsters, monster quickness to fire weapon, health)
 PX = 'px',
 idArea = '#area',
 idGame = '#game',
@@ -94,7 +108,9 @@ game = {
             for (x = 0; x < cols; x++) {
                 // create div cell          
                 var cls = 'c c', t = tiletypes.WALL;
-                if (0 === y || 0 === x || rows - 1 === y || cols - 1 === x || (x > 1 && x < cols - 2 && y === 5) || (x > 1 && x < cols - 2 && y === 3)) {
+
+                // Demo Setup
+                if (0 === y || 0 === x || rows - 1 === y || cols - 1 === x || (x > 1 && x < cols - 2 && y % 6 === 3) || (x > 1 && x < cols - 2 && y % 6 == 5)) {
                     if ((x === 0 || cols - 1 === x) && Math.floor(rows / 2) === y) {
                         cls += '3';
                         t = tiletypes.DOOR;
@@ -106,6 +122,7 @@ game = {
                     cls += '1';
                     t = tiletypes.FLOOR;
                 }
+
                 $(idGame).append(jqdiv('c' + (y * x), cls, '.', x * cellsize, y * cellsize, cellsize, cellsize));
 
                 tiles[y][x] = {
@@ -125,8 +142,8 @@ game = {
         for (var i = 0; i < numMonsters; ++i) {
             monsters[i] = {
                 id: 'm' + i,
-                x: cellsize * i + cellsize * 2,
-                y: cellsize * 2,
+                x: cols / 2 * cellsize + (i - numMonsters / 2) * cellsize,
+                y: rows / 2 * cellsize,
                 oldx: 50 * i + 100,
                 oldy: 100,
                 dx: 0,
@@ -136,7 +153,13 @@ game = {
                 attackPower: 2,
                 dir: 1 + M.floor(M.random() * directions.DOWN),
                 speed: 2,
+                health: rnd(1, 3),
                 state: spritestates.ALIVE,
+                aistate: aistates.RANDOM,
+                aiTimer: 0,
+                aiTimerThreshold: 100,
+                dirTimer: 0,
+                dirTimerThreshold: 100,
                 el: jqdiv('m' + i, 'm', 'M', 0, 0, cellsize, cellsize)
             };
             //log(monsters[i]);
@@ -242,20 +265,57 @@ game = {
 
             var monster = monsters[i];
             if (monster.state !== spritestates.DEAD) {
+
+                // check and change direction
+                monster.dirTimer += 1;
+                if (monster.dirTimer > monster.dirTimerThreshold) {
+
+                    monster.dirTimer = 0;
+
+                    // check and change ai state
+                    monster.aiTimer += 1;
+                    if (++monster.aiTimer > monster.aiTimerThreshold) {
+                        monster.aiTimer = 0;
+
+                        // TODO: Move the actual change of AI state to a method of monster class
+                        // monster.changeState()
+                        monster.aistate = rnd(1, n_aistates - 1);
+                    }
+
+                    // Move the actual change of direction selection to a method of monster class
+                    // monster.changeDirection()
+                    switch (monster.aistate) {
+                        case aistates.RANDOM:
+                            log('random');
+                            monster.dir = rnd(1, n_directions - 1);
+                            log('monster.dir=' + monster.dir);
+                            break;
+                        case aistates.CHASE:
+                            // get dir based on player position -- 
+                            // closest delta (x/y) will give orthogonal monster direction (deltax < deltay == UP/DOWN)
+                            log('chase');
+                            monster.dir = rnd(1, n_directions - 1);
+                            break;
+                        default:
+                            log('default');
+                            monster.dir = rnd(1, n_directions - 1);
+                            break;
+                    }
+                }
+
                 game.move(monster);
 
-                rnd = Math.floor(Math.random() * 50);
-                if (rnd == 25) {
+                if (rnd(0, 100) == 25) {
                     game.fireWeapon(monster);
                 }
 
-                //
+                // check for collisions
                 if (game.collideTile(monster, true)) {
                     monster.x -= 2 * monster.dx;
                     monster.y -= 2 * monster.dy;
-                    log("[before]monster.dir = " + monster.dir);
+                    //log("[before]monster.dir = " + monster.dir);
                     monster.dir = (monster.dir < directions.DOWN) ? monster.dir + 1 : directions.LEFT;
-                    log("[after]monster.dir = " + monster.dir);
+                    //log("[after]monster.dir = " + monster.dir);
                 }
 
                 game.update(monster, true);
@@ -274,10 +334,10 @@ game = {
 
         weapons.push({
             id: _id,
-            x: e.x,
-            y: e.y,
-            oldx: e.x,
-            oldy: e.y,
+            x: e.x + cellsize / 4,
+            y: e.y + cellsize / 4,
+            oldx: e.x + cellsize / 4,
+            oldy: e.y + cellsize / 4,
             dx: 0,
             dy: 0,
             w: cellsize / 2,
@@ -312,8 +372,11 @@ game = {
                     // if collide with monster, kill monster
                     var monster = game.collideMonster(w);
                     if (null !== monster) {
-                        game.killMonster(monster);
-                        game.killWeapon(w);
+                        monster.health -= player.attack;
+                        if (monster.health < 0) {
+                            game.killMonster(monster);
+                            game.killWeapon(w);
+                        }
                     }
                 } else {
                     // if collide with player, hurt player
